0a65ca3f8ac1480b2acb5777dd93872b9754f296
Alex Widener <awidener3@gmail.com>
2022-07-02 09:46:54 -0400

    Pushing to GitHub

--- script.js
+++ script.js
@@ -3,18 +3,22 @@ const last = document.querySelector('#last-name');
 const email = document.querySelector('#email');
 const phone = document.querySelector('#phone-number');
 const password = document.querySelector('#password');
-const confirm = document.querySelector('#confirm-password');
+const confirmPass = document.querySelector('#confirm-password');
 
+// Validation regex
 const nameRegex = /^[a-zA-Z]+$/;
 const emailRegex = /^[^@]+@[^@]+\.[^@]+$/;
 const phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
 
+// Listeners
 first.addEventListener('change', (e) => validate(nameRegex, e.target));
 last.addEventListener('change', (e) => validate(nameRegex, e.target));
 email.addEventListener('change', (e) => validate(emailRegex, e.target));
 phone.addEventListener('change', (e) => validate(phoneRegex, e.target));
-confirm.addEventListener('change', (e) => confirmPassword(e.target));
+password.addEventListener('change', (e) => confirmPassword(e.target));
+confirmPass.addEventListener('change', (e) => confirmPassword(e.target));
 
+// Validates based on regex statement
 function validate(regex, el) {
 	// Remove mark if present
 	if (el.parentNode.lastElementChild.classList.contains('fa-solid')) {
@@ -23,28 +27,51 @@ function validate(regex, el) {
 
 	if (!regex.test(el.value)) {
 		el.style.borderColor = 'red';
-		const check = document.createElement('i');
-		check.classList.add('fa-solid', 'fa-circle-xmark', 'xmark');
-		el.parentNode.insertBefore(check, el.nextSibling);
+		el.parentNode.insertBefore(createCheck('xmark'), el.nextSibling);
 	} else {
 		el.style.borderColor = 'green';
-		const check = document.createElement('i');
-		check.classList.add('fa-solid', 'fa-circle-check', 'checkmark');
-		el.parentNode.insertBefore(check, el.nextSibling);
+		el.parentNode.insertBefore(createCheck('checkmark'), el.nextSibling);
 	}
 }
 
+// Confirms value of password matches
 function confirmPassword(el) {
-	if (el.value === password.value) {
+	// Remove mark if present
+	if (el.parentNode.lastElementChild.classList.contains('fa-solid')) {
+		el.parentNode.lastElementChild.remove();
+		password.parentNode.lastElementChild.remove();
+	}
+
+	if (el === password) {
+		console.log('working with password');
+	} else if (el.value === password.value) {
 		password.style.borderColor = 'green';
 		el.style.borderColor = 'green';
+		el.parentNode.insertBefore(createCheck('checkmark'), el.nextSibling);
+		password.parentNode.insertBefore(
+			createCheck('checkmark'),
+			password.nextSibling
+		);
+	} else {
+		password.style.borderColor = 'red';
+		el.style.borderColor = 'red';
+		el.parentNode.insertBefore(createCheck('xmark'), el.nextSibling);
+		password.parentNode.insertBefore(
+			createCheck('xmark'),
+			password.nextSibling
+		);
+	}
+}
 
-		const check1 = document.createElement('i');
-		check1.classList.add('fa-solid', 'fa-circle-check', 'checkmark');
-		el.parentNode.insertBefore(check1, el.nextSibling);
-
-		const check2 = document.createElement('i');
-		check2.classList.add('fa-solid', 'fa-circle-check', 'checkmark');
-		password.parentNode.insertBefore(check2, password.nextSibling);
+// Creates a check or xmark element
+function createCheck(type) {
+	if (type === 'checkmark') {
+		const check = document.createElement('i');
+		check.classList.add('fa-solid', 'fa-circle-check', 'checkmark');
+		return check;
+	} else {
+		const check = document.createElement('i');
+		check.classList.add('fa-solid', 'fa-circle-xmark', 'xmark');
+		return check;
 	}
 }
